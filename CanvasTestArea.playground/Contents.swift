f2d89a145871724772b8aef9b78b68cdb75d7aa8
Liam Davidson <ldavidson@lcs-mba3335.lcs.on.ca>
2017-09-28 11:24:49 -0400

    Work on my Sun image.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,20 +21,18 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let canvas = Canvas(width: 300, height: 250)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
-
-// Show where the origin is
 canvas.drawAxes()
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+canvas.fillColor = Color.blue
 
-//Draw a rectangle at the top of the screen
-canvas.drawRectangle(centreX: 150, centreY: 400, width: 100, height: 50)
+Canvas.drawLine()
+
+
+canvas.drawRectangle(centreX: 150, centreY: 125, width: 300, height: 250, borderWidth: 0)
+
+
 
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
