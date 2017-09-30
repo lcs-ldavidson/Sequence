6c83c954be12fe6769f592b712dea761ae1bd3a7
Liam Davidson <ldavidson@lcs-mba3335.local>
2017-09-30 11:21:54 -0400

    Made cloud and sun

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -27,12 +27,30 @@ canvas.drawAxes()
 
 canvas.fillColor = Color.blue
 
-Canvas.drawLine()
-
 
 canvas.drawRectangle(centreX: 150, centreY: 125, width: 300, height: 250, borderWidth: 0)
 
+canvas.fillColor = Color.white
 
+canvas.borderColor = Color.white
+
+canvas.drawEllipse(centreX: 270, centreY: 250, width: 75, height: 75)
+
+canvas.drawEllipse(centreX: 300, centreY: 215, width: 75, height: 75)
+
+canvas.borderColor = Color.yellow
+canvas.fillColor = Color.yellow
+
+canvas.drawEllipse(centreX: 0, centreY: 250, width: 70, height: 70)
+
+canvas.drawLine(fromX: <#T##Int#>, fromY: <#T##Int#>, toX: <#T##Int#>, toY: <#T##Int#>)
 
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
+
+
+
+
+
+
+
