788659bb4fb1fa20d3bf2a407f2cf7be85a9bebd
Liam Davidson <ldavidson@lcs-mba3335.lcs.on.ca>
2017-10-03 14:57:03 -0400

    I FINISHED!!!!!!!!!!

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -25,7 +25,7 @@ let canvas = Canvas(width: 300, height: 250)
 
 canvas.drawAxes()
 
-canvas.fillColor = Color.blue
+canvas.fillColor = Color.init(hue:200, saturation: 100, brightness: 90, alpha: 70)
 
 
 canvas.drawRectangle(centreX: 150, centreY: 125, width: 300, height: 250, borderWidth: 0)
@@ -77,13 +77,59 @@ canvas.drawLine(fromX: 0, fromY: 250, toX: 125, toY: 0)
 
 canvas.drawLine(fromX: 0, fromY: 250, toX: 175, toY: 0)
 
-canvas.drawLine(fromX: 0, fromY: 250, toX: <#T##Int#>, toY: <#T##Int#>)
+canvas.drawLine(fromX: 0, fromY: 250, toX: 225, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 250, toX: 275, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 250, toX: 300, toY: 31)
+
+canvas.drawLine(fromX: 0, fromY: 250, toX: 300, toY: 93)
+
+canvas.drawLine(fromX: 0, fromY: 250, toX: 300, toY: 155)
+
+canvas.drawLine(fromX: 0, fromY: 250, toX: 300, toY: 217)
+
+//rainbow
+
+canvas.borderColor = Color.purple
+
+canvas.defaultBorderWidth = 15
+
+canvas.drawShapesWithFill = false
+
+canvas.drawEllipse(centreX: 150, centreY: 0, width: 140, height: 100)
+
+canvas.borderColor = Color.blue
+
+
+canvas.drawEllipse(centreX: 150, centreY: 0, width: 170, height: 130)
+
+canvas.borderColor = Color.green
+ canvas.drawEllipse(centreX: 150, centreY: 0, width: 200, height: 160)
+
+canvas.borderColor = Color.yellow
+
+canvas.drawEllipse(centreX: 150, centreY: 0, width: 230, height: 190)
+
+canvas.borderColor = Color.orange
+
+canvas.drawEllipse(centreX: 150, centreY: 0, width: 260, height: 220)
+
+canvas.borderColor = Color.red
+
+canvas.drawEllipse(centreX: 150, centreY: 0, width: 290, height: 250)
 
 
 
 
 
 
+
+canvas.defaultBorderWidth = 5
+
+canvas.drawShapesWithFill = true
+
+canvas.borderColor = Color.white
 canvas.drawEllipse(centreX: 270, centreY: 250, width: 75, height: 75)
 
 canvas.drawEllipse(centreX: 300, centreY: 215, width: 75, height: 75)
